3bcfddf883acdc921f72b0e5d1f0532d6bf8a6d8
rancor <rancor.dpacifist@gmail.com>
2022-08-01 16:16:09 +0800

    I hope tis works

--- javascript.js
+++ javascript.js
@@ -1,20 +1,32 @@
-const time = new Date().getHours()
-let jikan;
-if (time < 10) {
-    jikan = 'Good morning!'
-}
-else if (time < 12) {
-    jikan =  'Good noon'
-}
-else if (time < 4) {
-    jikan = 'Good Afternoon'
-}
-else if (time < 20) {
-    jikan = 'Good Evening'
-}
-else {
-    jikan = 'Good Night'
-}
 
-console.log(time)
-console.log(jikan)
+let response;
+let score = 80;
+let machineActive = false;
+
+
+let fail = score > 100 && score < 0
+let almost = score >= 0 && score <= 19
+let barely = score >= 20 && score <= 39
+let nice = score >= 40 && score <= 69
+let awesome = score >= 70 && score <= 89
+let howTheHell = score >= 90 && score <= 100
+
+switch (machineActive) {
+    case "true":
+        //my whole command here
+        break;
+case "false":
+    response = "Machine is not working"
+}
+// Don't edit the code below here!
+
+const section = document.querySelector('section');
+
+let para1 = document.createElement('p');
+let para2 = document.createElement('p');
+
+para1.textContent = `Your score is ${score}`;
+para2.textContent = response;
+
+section.appendChild(para1);
+section.appendChild(para2);
