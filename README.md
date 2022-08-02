f4675bed0ac26000aaf6754da05b0e34c421a139
rancor <rancor.dpacifist@gmail.com>
2022-08-02 17:27:02 +0800

    Finish lesson 5

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 
 I think I should put my updates here and to mark my progress.
 A little too late to do this but hey, its always better late than never
+
+Conditionals 
+1 done
+2 done 
+3 done
+4 done
+5 ongoing
---
+++ javascript.js
@@ -0,0 +1,14 @@
+let message = prompt("Who?")
+let login;
+
+message = (login == "Employee") ? "Hello" : 
+(login == "Director") ? "Greetings" : 
+(login == "") ? "No login" : "";
+console.log(message)
+
+message = (login == 'Employee') ? 'Hello' :
+  (login == 'Director') ? 'Greetings' :
+  (login == '') ? 'No login' : '';
+  console.log(message)
+
+  //nonononononnoononononononononononononon XXXXXXXXXXXXXXXXXx
